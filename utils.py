bc367f9e6a90179d38f37a19cf4c8eb066806ab7
khaloumohammed <164230583+khaloumohammed@users.noreply.github.com>
2025-09-07 11:37:51 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+# Mettez cette fonction dans un fichier utilitaire, par exemple 'utils.py'
+import flet as ft
+
+def afficher_snack_bar(page, classe):
+    # Crée un objet SnackBar avec le message désiré
+    snack_bar = ft.SnackBar(
+        content=ft.Text(f"Vous avez sélectionné la classe : {classe}"),
+        action="Fermer",
+        on_action=lambda e: print("SnackBar fermé."), # Action optionnelle
+    )
+    
+    # Attache le SnackBar à la page
+    page.snack_bar = snack_bar
+    
+    # Affiche le SnackBar
+    #page.open(snack_bar)
+    page.snack_bar.open=True
+    page.update()
